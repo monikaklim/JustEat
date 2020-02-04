5b4ce362e60815f095dd990e3bb495c32381ad80
monikaklim <monyklim@gmail.com>
2020-02-04 15:35:22 +0100

    successfully rendering products fetched from just eat

--- src/components/Menu/Products/Products.js
+++ src/components/Menu/Products/Products.js
@@ -13,9 +13,36 @@ state ={
 
 componentDidMount(){
   
-axios.get('https://www.justeat.it/menu/getproductsformenu?menuId=37633').then(response => {console.log(response)
+axios.get('https://www.justeat.it/menu/getproductsformenu?menuId=37633').then(response => {
+    console.log(response.data);
+    
+let cat = [];
+ let sau = [];
+ let prod = [];
+
+ cat = response.data.Menu.Categories.map(c =>
+  {return c.Name }
+  );
+  
+  sau = response.data.Menu.accessories.map(a=>
+  {return a.Name }
+  );
+
+  prod = response.data.Menu.products.map((p,index)=>
+    {return (
+  <Product key = {index} name = {p.Name} desc = {p.Desc}  price = {p.Price} /> 
+            );
+
+    
+    }
+    );
+  
+  
 
 
+
+  this.setState({products: prod, categories: cat, sauces: sau});
+  console.log(this.state);
 }
 
 
@@ -25,21 +52,14 @@ axios.get('https://www.justeat.it/menu/getproductsformenu?menuId=37633').then(re
 }
 
 
-getProductsHandler = () =>{
-
-axios.get('https://www.justeat.it/menu/getproductsformenu?menuId=37633').then(response => {console.log(response)});
-
-}
-
-
 
 
 render(){
 
     return(
 <div>     
+{this.state.products}
 
-<Product name = "nome" desc = "test" price = "0"/>
 </div>   
 
     );
