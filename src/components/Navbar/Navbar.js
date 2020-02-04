4ba8e7082be623fcdb9b37c69ab609eb17d0b348
monikaklim <monyklim@gmail.com>
2020-02-04 14:14:11 +0100

    created Navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './Navbar.module.css';
 
-const navbar = () =>{
-
-
-<ul>
-
-<li>  <Link>Home</Link></li>
-<li>  <Link>Menù</Link></li>
+const navbar = () =>(
 
 
+<ul className = "Navbar">
+    <li >  <Link className = "Link" to = "/menu" >Menù</Link></li>
+    <li >  <Link className = "Link" to= "/" >Home</Link></li>
 </ul>
 
 
-
-};
+);
 export default navbar;
